ef56202597dda58acaf22257b2cd1fdde8f5ce7e
OLODiman11 <OLODiman11@mail.ru>
2024-02-25 15:12:33 +0300

    GridConfig resource

--- scenes/shape_previews/preview_box.gd
+++ scenes/shape_previews/preview_box.gd
@@ -11,6 +11,8 @@ func show_preview(types: Array[TetrisShape.Type]):
 	var y := 0
 	for type in types:
 		var preview := ShapePreview.new(type)
-		preview.position.y = y
-		y += preview.get_height() + gap
+		var bounds := preview.get_bounds()
+		preview.position.x = - bounds[0] - bounds[2] / 2.0
+		preview.position.y = y - bounds[1]
+		y += bounds[3] + gap
 		add_child(preview)
--- scenes/shape_previews/shape_preview.gd
+++ scenes/shape_previews/shape_preview.gd
@@ -1,18 +1,9 @@
-@tool
-
 class_name ShapePreview
 
 extends Node2D
 
-@export var tile_size: int = 39
-@export var tile_margin: int = 2
-@export var type: TetrisShape.Type:
-	set(value):
-		type = value
-		update_shape()
-
-@export var colors: Array[Color] = [
-	Color.TRANSPARENT, Color.RED, Color.ORANGE, Color.YELLOW, Color.GREEN, Color.BLUE, Color.DARK_BLUE, Color.PURPLE]
+@export var config: GridConfig = preload("res://resources/grid_config.tres")
+@export var type: TetrisShape.Type
 
 func _init(type: TetrisShape.Type):
 	self.type = type
@@ -21,18 +12,21 @@ func _init(type: TetrisShape.Type):
 func _ready():
 	update_shape()
 
-func get_height() -> int:
-	return tile_size * TetrisShape.new(type).get_size()
+func get_bounds() -> Vector4:
+	var bounds := config.tile_size * TetrisShape.new(type).get_actual_bounds()
+	return Vector4(bounds[0], bounds[1], 
+		bounds[2] - bounds[0] + config.tile_size, 
+		bounds[3] - bounds[1] + config.tile_size)
 
 func update_shape():
 	for child in get_children():
 		remove_child(child)
 	var shape = TetrisShape.new(type).get_shape()
 	var shape_size = shape.size()
-	var size = tile_size - 2 * tile_margin
+	var size := config.tile_size - 2 * config.tile_margin
 	for row in range(shape_size):
 		for col in range(shape_size):
-			var color = colors[shape[row][col]]
-			var x = col * tile_size + tile_margin
-			var y = row * tile_size + tile_margin
+			var color = config.colors[shape[row][col]]
+			var x = col * config.tile_size + config.tile_margin
+			var y = row * config.tile_size + config.tile_margin
 			add_child(Tile.new(color, Vector2i(x, y), Vector2i(size, size)))
